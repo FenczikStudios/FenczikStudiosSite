7a23f4cf3381d80b193cc5d27aca7bbecb29fbd2
FenczikStudios <steven@fenczikstudios.com>
2023-04-15 17:18:30 -0600

    email button working?

--- FenczikStudios.js
+++ FenczikStudios.js
@@ -101,8 +101,12 @@ for (let i = 0; i < buttons.length; i++) {
 
 //MAIL CHIMP:
 function showEmailListForm () {
-  document.getElementById("email-list-form-container").style.display = "block";
+  !function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/aa26a1d185413f1f3a8b62e2b/0be3608a4945bce34f913e553.js");
 }
+
+//unsetting the cookie
+document.cookie = "MCPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";                  
+document.cookie = "MCPopupSubscribed=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
 
 // create a Lottie animation
